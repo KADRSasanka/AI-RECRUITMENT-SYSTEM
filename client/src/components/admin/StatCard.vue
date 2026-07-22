dfb47f1ac9d391902491733e82da775b16de436c
Tharindu Madarasingha <tharindu.rashmika385@gmail.com>
2026-07-23 01:09:37 +0530

    kkkk

---
+++ client/src/components/admin/StatCard.vue
@@ -0,0 +1,37 @@
+<template>
+
+<div class="bg-white rounded-xl shadow p-6">
+
+    <h3 class="text-gray-500 text-sm">
+        {{title}}
+    </h3>
+
+
+    <p class="text-4xl font-bold mt-3">
+        {{value}}
+    </p>
+
+
+</div>
+
+</template>
+
+
+<script setup>
+
+defineProps({
+
+    title:{
+        type:String,
+        required:true
+    },
+
+
+    value:{
+        type:[String,Number],
+        required:true
+    }
+
+});
+
+</script>
