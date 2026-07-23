d71b349483b315c31ec9ed88f39900fba231e098
Ravindu Gunarathne <adrsgunarathne@students.nsbm.ac.lk>
2026-07-24 00:28:29 +0530

    abcd

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbeb25934187c19dc56b0f8327c791a81e47a9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe2a6131d383fe18f35e98129ad08df3945781b")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
