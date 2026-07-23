09d8b0c94196a364e3c7928fffa78bd236f492bc
wadrkavindi <wadrkavindi@students.nsbm.ac.lk>
2026-07-23 20:41:52 +0530

    Raveesha

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08464e3b139ba9773c7e0585f4b1b86abe7f78be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0430476cfbc0ce7bfe4e9d1e4217a8dc8c854816")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
