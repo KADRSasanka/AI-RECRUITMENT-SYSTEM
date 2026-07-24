e1998bf5ff101d4d7043e669acf4ab4b79f3d5a2
Tharindu Madarasingha <tharindu.rashmika385@gmail.com>
2026-07-24 10:48:54 +0530

    npm install

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14f6497787042c8b7b86acf0c6200359e2ad133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8fd799d500b9eaf98de06bb683a755b05957c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
